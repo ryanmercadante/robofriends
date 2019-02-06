d61c573ba4d6fc67273f0a4b7f2eb02593ec9392
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-06 17:43:01 -0500

    create first component and install tachyons

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>This is my first react component</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
