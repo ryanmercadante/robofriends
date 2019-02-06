0e3dba333f7729a2be168183a440947631d5980f
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-06 18:17:28 -0500

    delete unnecessary files and create card component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots'
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
