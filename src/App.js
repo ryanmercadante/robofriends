c8fea34934dd6334274f80ee33644673cc7af0f8
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-07 09:43:42 -0500

    create function to filter through robots as you enter input in search box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots'
 import SearchBox from './SearchBox';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={ robots } />
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    // Because App owns state, including robots, its allowed to change it.
+    super();
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={ this.onSearchChange }/>
+        <CardList robots={ filteredRobots } /> {/*  CardList accepts robots as props even though its state in App.js */}
+      </div>
+    );
+  }
 }
 
 export default App;
