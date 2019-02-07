599dd66b523cabc899022863377d80e4adcb8a1d
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-07 08:42:37 -0500

    create cardlist component to list out all the robot cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots'
 
-ReactDOM.render(<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>, document.getElementById('root'));
+ReactDOM.render(<CardList robots={ robots } />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
