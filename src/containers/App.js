1af11d2cfb4ee82bc8f654f5667f82d41800f3d0
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-19 22:37:21 -0500

    cleanup file structure and add ErrorBoundry component

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    // Because App owns state, including robots, its allowed to change it.
+    super();
+    this.state = {
+      robots: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />{" "}
+              {/*  CardList accepts robots as props even though its state in App.js */}
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
