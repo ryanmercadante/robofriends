77ca8b4c90eafa34a9ecabadc51a2e687728c3f7
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-20 00:21:40 -0500

    setup connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+// dispatch is what triggers the action
+const mapDispatchToProps = dispatch => {
+  return {
+    // onSearchChange prop receives an event, and this event is going to dispatch the action setSearchField which receives input text, hence event.target.value
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
+
 class App extends Component {
   constructor() {
     // Because App owns state, including robots, its allowed to change it.
     super();
     this.state = {
-      robots: [],
-      searchField: ""
+      robots: []
     };
   }
 
@@ -21,23 +37,19 @@ class App extends Component {
       .then(users => this.setState({ robots: users }));
   }
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
-
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
+    if (robots.length === 0) {
       return <h1>Loading...</h1>;
     } else {
       return (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundry>
               <CardList robots={filteredRobots} />{" "}
@@ -50,4 +62,9 @@ class App extends Component {
   }
 }
 
-export default App;
+// connect is a higher order function - a function that returns another function
+// connect is going to run and whatever connect does inside the function is going to return another function which runs App
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
